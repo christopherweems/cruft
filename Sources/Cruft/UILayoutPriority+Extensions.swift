0df93bfa46a8036592ccdaf48b72fda61aa35fe9
Christopher Weems <hello@christopherweems.com>
2020-09-19 16:18:46 -0400

    Add UILayoutPriority.Level

---
+++ Sources/Cruft/UILayoutPriority+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UILayoutPriority+Extensions.swift
+//  
+//
+//  Created by Christopher Weems on 9/19/20.
+//
+
+import UIKit
+
+public extension UILayoutPriority {
+    typealias Level = Float
+    
+}
