d2b7ac39887b22ebce26a6bbc1d949389e361e11
Christopher Weems <hello@christopherweems.com>
2020-09-19 16:18:33 -0400

    Add `UIView.opacity`

---
+++ Sources/Cruft/UIView+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+Extensions.swift
+//  
+//
+//  Created by Christopher Weems on 9/19/20.
+//
+
+import UIKit
+
+public extension UIView {
+    var opacity: CGFloat {
+        get { alpha }
+        set { alpha = newValue }
+    }
+    
+}
